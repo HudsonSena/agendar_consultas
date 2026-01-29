c67a1d95888a2d1795d6d9038688a3117d4d904d
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2026-01-29 19:55:00 -0300

    update project

--- agenda-medica/prisma/migrations/20260129224752_init/migration.sql
+++ agenda-medica/prisma/migrations/20260129224752_init/migration.sql
@@ -1,14 +1,16 @@
 -- CreateTable
 CREATE TABLE "Appointment" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "age" INTEGER NOT NULL,
     "phone" TEXT NOT NULL,
     "cpf" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "date" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
