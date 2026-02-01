661c1ef9fdf3149c346e49353dc31c84d83fb82f
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2026-02-01 01:07:44 -0300

    update with erros in user.create

--- agenda-medica/src/app/actions/auth.ts
+++ agenda-medica/src/app/actions/auth.ts
@@ -1,29 +1,30 @@
-import { SignupFormSchema, FormState } from '../../lib/definitions.js'
-import prisma from '../../lib/prisma.js'
-import bcrypt from 'bcrypt'
- 
+"use server";
+import { SignupFormSchema, FormState } from "../../lib/definitions";
+import prisma from "../../lib/prisma";
+import bcrypt from "bcrypt";
+
 export async function signup(state: FormState, formData: FormData) {
   // Validate form fields
   const validatedFields = SignupFormSchema.safeParse({
-    username: formData.get('username'),
-    name: formData.get('name'),
-    email: formData.get('email'),
-    password: formData.get('password'),
-  })
- 
+    username: formData.get("username"),
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
   // If any form fields are invalid, return early
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
-    }
+    };
   }
- 
+
   // 2. Prepare data for insertion into database
-  const { username, name, email, password } = validatedFields.data
+  const { username, name, email, password } = validatedFields.data;
   // e.g. Hash the user's password before storing it
-  const hashedPassword = await bcrypt.hash(password, 10)
- 
-  // 3. Insert the user into the database or call an Auth Library's API 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // 3. Insert the user into the database or call an Auth Library's API
   const newUser = await prisma.user.create({
     data: {
       username,
@@ -31,14 +32,15 @@ export async function signup(state: FormState, formData: FormData) {
       email,
       password: hashedPassword,
     },
-  })
- 
-  const user = newUser[0]
- 
+  });
+
+  const user = newUser[0];
+  console.log("New user created:", user);
+
   if (!user) {
     return {
-      message: 'An error occurred while creating your account.',
-    }
+      message: "An error occurred while creating your account.",
+    };
   }
   // Call the provider or db to create a user...
 }
--- agenda-medica/src/app/page.tsx
+++ agenda-medica/src/app/page.tsx
@@ -3,32 +3,9 @@ import { Item } from "../components/item";
 import prisma from "../lib/prisma";
 import Link from "next/link";
 import { EventDay } from "../components/diaconsulta";
+import SignupForm from "../components/signup";
 
 export default async function Home() {
-  const agendados = await prisma.appointment.findMany();
-  return (
-    <div className="sm:w-85 m-auto my-5 flex flex-col items-center justify-center px-4 gap-3">
-      <div className="w-full flex flex-row justify-between">
-        <h1 className="text-2xl font-bold">Agenda de Consultas</h1>
-        <Button variant={"outline"} className="font-semibold">
-          Sair
-        </Button>
-      </div>
-      <div className="w-full flex flex-row justify-between items-center">
-        <EventDay date={new Date()} />
-        <Link
-          href={"/agendar"}
-          className="w-full border-2 rounded-md py-2 flex items-center justify-center hover:bg-slate-200 font-semibold text-blue-600"
-        >
-          Agendar Consulta
-        </Link>
-      </div>
-      <div>
-        <h1>Total de: {agendados.length} consultas.</h1>
-        {agendados.map((item) => (
-          <Item data={item} key={item.id} />
-        ))}
-      </div>
-    </div>
-  );
+  //const agendados = await prisma.appointment.findMany();
+  return <SignupForm />;
 }
